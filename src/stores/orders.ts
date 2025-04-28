54c3971217eae7215ec28f47d763b00385e2065a
Sefa GUCLU <sefa.guclu@eolas.fr>
2025-04-28 17:04:18 +0200

    Implement orders store with CRUD operations and define Order type

---
+++ src/stores/orders.ts
@@ -0,0 +1,69 @@
+import api from '@/services/api';
+import type { Order } from '@/types/Order';
+import { defineStore } from 'pinia';
+
+const url = '/orders';
+
+export const useOrdersStore = defineStore({
+  id: 'orders',
+  state: () => ({
+    orders: [] as Order[],
+    order: {} as Order
+  }),
+  actions: {
+    async fetchOrders(userId: string) {
+      try {
+        const response = await api.get(`${url}/${userId}`);
+        this.orders = response.data;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+    async fetchOrder(id: string) {
+      try {
+        const response = await api.get(`${url}/${id}`);
+        this.order = response.data;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+    async createOrder(order: Order) {
+      try {
+        const response = await api.post(url, order);
+        this.orders.push(response.data);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+    async closeOrder(id: string) {
+      try {
+        await api.post(`${url}/close/${id}`);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+    async updateOrderStatus(id: string, status: string) {
+      try {
+        await api.put(`${url}/${id}/status`, { status });
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+    async deleteOrder(id: string) {
+      try {
+        await api.delete(`${url}/${id}`);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+    resetOrder() {
+      this.order = {} as Order;
+    }
+  }
+});
---
+++ src/types/Order.ts
@@ -0,0 +1,7 @@
+export interface Order {
+  id: string;
+  client_id: string;
+  order_status: string;
+  created_at: string;
+  updated_at: string;
+}
